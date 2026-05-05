224738e13f13545614ff72bd5f0d115ea2483bd1
Bello Qudus <belloqudus3@gmail.com>
2026-05-05 02:51:09 -0700

    More codes

--- obj/Debug/net10.0/CRUD APP MVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/CRUD APP MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD APP MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97d9a47bef730f9b32dc825b40988e143a822a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD APP MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD APP MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
